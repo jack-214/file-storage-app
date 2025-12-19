4b4cbf333ce5108f0eebb53201135ef89d7dc605
Jack Ho <jack.ho12345@gmail.com>
2025-12-19 16:44:35 +0000

    feat: add Tailwind CSS configuration with custom theme and animations

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type FormType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
