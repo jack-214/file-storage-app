7a42c9c1edcf67abc048687980c6c0c9e511e01f
Jack Ho <jack.ho12345@gmail.com>
2025-12-21 20:39:23 +0000

    feat: implement form components with validation using react-hook-form and zod

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -29,9 +29,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </section>
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
-        <div className="mb-16 lg:hidden border border-black">
+        <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/logo-full-brand.svg"
             alt="Logo"
             width={224}
             height={82}
